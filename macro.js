ccd764c8e945358dbc28afe15949e63fd06751b5
Brad Cornes <bradlc41@gmail.com>
2018-08-03 10:50:27 +0100

    [#1] map named imports to php functions

--- macro.js
+++ macro.js
@@ -3,44 +3,68 @@ let path = require('path')
 let deasync = require('deasync')
 let { createMacro } = require('babel-plugin-macros')
 let { parseExpression } = require('babylon')
+let generate = require('babel-generator').default
 
 module.exports = createMacro(tinkerMacro)
 
 function tinkerMacro({ references, state, babel }) {
   let { types: t } = babel
 
-  if (typeof references.default === 'undefined') return
-
-  references.default.forEach(ref => {
-    if (ref.parentPath.type !== 'TaggedTemplateExpression') return
-
-    let php = ref.parentPath.node.quasi.quasis[0].value.cooked
-
-    let tinker = spawn('php', [path.resolve(state.cwd, 'artisan'), 'tinker'])
-    tinker.stdin.setEncoding('utf-8')
-
-    let done = false
-    let result
-
-    tinker.stdout.on('data', data => {
-      if (data.toString().startsWith('=>')) {
-        result = data.toString().match(/=> (.*?)\n/)[1]
-        result = result.substr(1, result.length - 2)
-        done = true
-      }
-      if (done) {
-        tinker.stdin.end()
-        return
-      }
-      if (data.toString().startsWith('>>>')) {
-        tinker.stdin.write(`json_encode(${php})\n`)
-      }
+  references.default &&
+    references.default.forEach(ref => {
+      if (ref.parentPath.type !== 'TaggedTemplateExpression') return
+      let php = ref.parentPath.node.quasi.quasis[0].value.cooked
+      let result = evaluate(php, state)
+      ref.parentPath.addComment('trailing', 'tinker.macro')
+      ref.parentPath.replaceWith(parseExpression(result))
     })
 
-    deasync.loopWhile(() => !done)
+  Object.keys(references)
+    .filter(x => x !== 'default')
+    .forEach(x => {
+      references[x].forEach(ref => {
+        ref.parentPath.traverse({
+          ObjectExpression(path) {
+            let obj = generate(path.node).code
+            eval(`obj = ${obj}`)
+            path.replaceWith(
+              t.callExpression(t.identifier('json_decode'), [
+                t.stringLiteral(JSON.stringify(obj)),
+                t.booleanLiteral(true)
+              ])
+            )
+          }
+        })
+        let result = evaluate(generate(ref.parentPath.node).code, state)
+        ref.parentPath.addComment('trailing', 'tinker.macro')
+        ref.parentPath.replaceWith(parseExpression(result))
+      })
+    })
+}
 
-    ref.parentPath.addComment('trailing', 'tinker.macro')
+function evaluate(php, state) {
+  let tinker = spawn('php', [path.resolve(state.cwd, 'artisan'), 'tinker'])
+  tinker.stdin.setEncoding('utf-8')
 
-    ref.parentPath.replaceWith(parseExpression(result))
+  let done = false
+  let result
+
+  tinker.stdout.on('data', data => {
+    if (data.toString().startsWith('=>')) {
+      result = data.toString().match(/=> (.*?)\n/)[1]
+      result = result.substr(1, result.length - 2)
+      done = true
+    }
+    if (done) {
+      tinker.stdin.end()
+      return
+    }
+    if (data.toString().startsWith('>>>')) {
+      tinker.stdin.write(`json_encode(${php})\n`)
+    }
   })
+
+  deasync.loopWhile(() => !done)
+
+  return result
 }
